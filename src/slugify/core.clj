d8d10852ec3f24232591b114cc377ed2647b0f2e
Dave Liepmann <dave.liepmann@gmail.com>
2017-05-15 17:36:32 +0200

    Fix indentation

--- src/slugify/core.clj
+++ src/slugify/core.clj
@@ -8,16 +8,16 @@
 (defn- normalize [string-to-normalize]
   (let [normalized (Normalizer/normalize string-to-normalize Normalizer$Form/NFD)
         ascii (s/replace normalized #"[\P{ASCII}]+" "")]
-        (s/lower-case ascii)))
+    (s/lower-case ascii)))
 
 (defn slugify
-  "Returns a slugified string. Takes two optional parameters: 
-      delimiter (str): string that interleaves valid words,
-      trim-value (int): max url value."
-  ([string-to-slugify] (slugify string-to-slugify "-"))
+  "Returns a slugified string. Takes two optional parameters:
+  delimiter (str): string that interleaves valid words,
+  trim-value (int): max url value."
+  ([string-to-slugify]           (slugify string-to-slugify "-"))
   ([string-to-slugify delimiter] (slugify string-to-slugify delimiter 250))
   ([string-to-slugify delimiter trim-value]
-    (let [normalized (normalize string-to-slugify)
-          split-s (s/split (s/triml normalized) #"[\p{Space}\p{P}]+")
-          combined (s/join delimiter split-s)]
-      (trim-to combined trim-value))))
+   (let [normalized (normalize string-to-slugify)
+         split-s (s/split (s/triml normalized) #"[\p{Space}\p{P}]+")
+         combined (s/join delimiter split-s)]
+     (trim-to combined trim-value))))
